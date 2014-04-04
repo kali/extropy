2661196344fed68d4adfdc3c6a0edef2d9455fb6
Mathieu Poumeyrol <kali@fotonauts.com>
2014-04-04 15:05:31 +0200

    switch to my favourite DB

--- extropy-web/app/controllers/Application.scala
+++ extropy-web/app/controllers/Application.scala
@@ -8,6 +8,7 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 
+import com.mongodb.casbah.Imports._
 
 object Application extends Controller {
 
@@ -20,21 +21,21 @@ object Application extends Controller {
     }
 
     def tasks = Action {
-        Ok(views.html.index(Task.all(), taskForm))
+        Ok(views.html.index(Task.findAll.toList, taskForm))
     }
 
     def newTask = Action { implicit request =>
         taskForm.bindFromRequest.fold(
-            errors => BadRequest(views.html.index(Task.all(), errors)),
+            errors => BadRequest(views.html.index(Task.findAll.toList, errors)),
             label => {
-                Task.create(label)
+                Task.save(Task(new ObjectId, label))
                 Redirect(routes.Application.tasks)
             }
         )
     }
 
-    def deleteTask(id: Long) = Action {
-        Task.delete(id)
+    def deleteTask(id:ObjectId) = Action {
+        Task.removeById(id)
         Redirect(routes.Application.tasks)
     }
 }
--- extropy-web/build.sbt
+++ extropy-web/build.sbt
@@ -6,8 +6,14 @@ libraryDependencies ++= Seq(
   jdbc,
   anorm,
   cache,
+  "se.radley" %% "play-plugins-salat" % "1.4.0",
   "org.webjars" %% "webjars-play" % "2.2.1-2",
   "org.webjars" % "bootstrap" % "3.1.0"
 )
 
 play.Project.playScalaSettings
+
+routesImport += "se.radley.plugin.salat.Binders._"
+
+templatesImport += "org.bson.types.ObjectId"
+
