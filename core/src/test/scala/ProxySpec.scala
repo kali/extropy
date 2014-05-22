327d1a389542efc16bfd8ea7a5d046ddd8422e99
Mathieu Poumeyrol <kali@fotonauts.com>
2014-05-22 13:40:42 +0200

    one more test on push

--- core/src/test/scala/ProxySpec.scala
+++ core/src/test/scala/ProxySpec.scala
@@ -59,6 +59,8 @@ class ProxySpec extends FlatSpec with Matchers with BeforeAndAfterAll with Extro
         allRules.foreach { rule => rule.checkAll(extropy.payloadMongo) should be ('empty) }
         proxy.doChange(setAuthorIdOnCommentsNestedSel)
         allRules.foreach { rule => rule.checkAll(extropy.payloadMongo) should be ('empty) }
+        proxy.doChange(pushCommentInPost1)
+        allRules.foreach { rule => rule.checkAll(extropy.payloadMongo) should be ('empty) }
     }
 
     it should "deal with deletes" in withExtropyAndBlog { (extropy, fixture) =>
