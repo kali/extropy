b0bafc89995384bdefe84fdb84ed08a5088e8c8b
Mathieu Poumeyrol <kali@fotonauts.com>
2014-06-13 15:35:25 +0200

    get a replica set up and running for tests

--- agent/src/test/scala/ReplicaSet.scala
+++ agent/src/test/scala/ReplicaSet.scala
@@ -5,7 +5,17 @@
  */
 package org.zoy.kali.extropy
 
+import java.net.InetSocketAddress
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
 import org.scalatest._
+import org.scalatest.concurrent.Eventually
+import org.scalatest.time._
+
+import akka.actor.{ ActorSystem, Actor, ActorRef, Props, PoisonPill }
+import akka.testkit.{ TestKit, TestActor }
 
 import de.flapdoodle.embed.mongo.{ MongodExecutable, MongodProcess, MongodStarter, Command }
 import de.flapdoodle.embed.process.runtime._
@@ -14,12 +24,16 @@ import de.flapdoodle.embed.mongo.config._
 import de.flapdoodle.embed.mongo.distribution.{ IFeatureAwareVersion, Version }
 
 import com.mongodb.casbah.Imports._
+import org.bson.BSONObject
+import mongoutils.BSONObjectConversions._
+
+import scala.collection.mutable.Buffer
 
 class ReplicaSet {
 
     def mongoWantedVersion:Option[IFeatureAwareVersion] = None
 
-    var ports:Array[Int] = Array.fill(3)(-1)
+    val ports:Buffer[Int] = Buffer.fill(3)(-1)
     var mongoExecutables:Array[MongodExecutable] = Array.fill(3)(null)
     var mongoProcesses:Array[MongodProcess] = Array.fill(3)(null)
 
@@ -28,7 +42,7 @@ class ReplicaSet {
     val runtime:MongodStarter = {
         val runtimeConfig = new RuntimeConfigBuilder()
                                 .defaults(Command.MongoD)
- //                               .processOutput(ProcessOutput.getDefaultInstanceSilent())
+                                .processOutput(ProcessOutput.getDefaultInstanceSilent())
                                 .build
         MongodStarter.getInstance(runtimeConfig)
     }
@@ -54,19 +68,74 @@ class ReplicaSet {
         Thread.sleep(1000)
     }
 
+    def up = {
+        val s = status
+        s.as[List[BSONObject]]("members").count( _.as[String]("stateStr") == "PRIMARY") == 1 &&
+        s.as[List[BSONObject]]("members").count( _.as[String]("stateStr") == "SECONDARY") == 2
+    }
+
+    def waitForUp(limit:Duration) {
+        val interval = 1 seconds
+        val started = System.currentTimeMillis
+        while(!up && (System.currentTimeMillis - started) < limit.toMillis) {
+            Thread.sleep(interval.toMillis)
+        }
+    }
+
     def stop {
         mongoProcesses.filter( _ != null).foreach( _.stop )
         mongoExecutables.filter( _ != null).foreach( _.stop )
     }
+
+    def status = mongoBackendClient("admin").command("replSetGetStatus")
 }
 
-class SomeSpec extends FlatSpecLike {
-    behavior of "Replica Set"
+class ReplicaSetSupportSpec extends TestKit(ActorSystem("replicaset"))
+        with FlatSpecLike with Eventually with Matchers with BeforeAndAfterAll {
 
-    it should "start a working replica set" taggedAs(Tag("k")) in {
-        val replicaset = new ReplicaSet
-        replicaset.start
+    val replicaset = new ReplicaSet
+    val proxiesPorts:Buffer[Int] = Buffer.fill(3)(-1)
+    val proxies:Buffer[ActorRef] = Buffer.fill(3)(null)
+
+    override def beforeAll { replicaset.start ; replicaset.waitForUp(1 minute) ; startProxies }
+    override def afterAll { replicaset.stop }
+
+    behavior of "Replica set testing prerequisites"
+
+    it should "start a working replica set" in {
         val conf = replicaset.mongoBackendClient("local")("system.replset").findOne().get
-        replicaset.stop
+        conf.as[List[_]]("members") should have size(3)
+        val status = replicaset.status
+        status.as[List[BSONObject]]("members").count( _.as[String]("stateStr") == "PRIMARY") should be(1)
+        status.as[List[BSONObject]]("members").count( _.as[String]("stateStr") == "SECONDARY") should be(2)
     }
+
+    it should "start a proxy for each member" taggedAs(Tag("k")) in {
+    }
+
+    def startProxies {
+        val now = System.currentTimeMillis
+        val payloadDbName = s"extropy-spec-payload-$now"
+        val extropyDbName = s"extropy-spec-internal-$now"
+        val fixture = BlogFixtures(payloadDbName)
+        Thread.sleep(1000)
+
+        replicaset.status.as[List[BSONObject]]("members").zipWithIndex.foreach { case (obj,index) =>
+            val backendLocation = obj.as[String]("name")
+            val backendPort = backendLocation.split(':').last.toInt
+            val extropy = ExtropyContext(replicaset.mongoBackendClient(extropyDbName), replicaset.mongoBackendClient)
+            proxiesPorts(index) = de.flapdoodle.embed.process.runtime.Network.getFreeServerPort
+            proxies(index) = system.actorOf(ProxyServer.props(
+                extropy,
+                new InetSocketAddress("127.0.0.1", proxiesPorts(index)),
+                new InetSocketAddress("127.0.0.1", backendPort)
+            ))
+        }
+        eventually {
+            replicaset.mongoBackendClient(extropyDbName)("agents") should have size(3)
+        }
+    }
+
+    implicit override val patienceConfig =
+        PatienceConfig(timeout = scaled(Span(60, Seconds)), interval = scaled(Span(2000, Millis)))
 }
